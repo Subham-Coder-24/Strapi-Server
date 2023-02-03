9e6c2f3c6bbef5c6aa69b7c8ba563e6b42625395
Subham Swarup Pradhan <85586258+Subham-Coder-24@users.noreply.github.com>
2023-02-03 19:12:01 +0530

    second

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cfef6fkgqg46rpnt12d0-a.oregon-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'sample_sw3a'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', '0nSiRwoOGO7JIznfRR55SKjwhutZQB7m'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT' ),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
